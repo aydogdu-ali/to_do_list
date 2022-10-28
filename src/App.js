1000a32d1bf855f19cce5b278bf9b4ecace09e41
Ali <ali.ozgur@example.com>
2022-10-29 01:16:44 +0300

    added picture and button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="container">
+    <div className="container App">
             <Header/>
             <Home/>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import Addtodo from '../components/Addtodo'
-import Listtodo from '../components/Listtodo'
+import React from "react";
+import Addtodo from "../components/Addtodo";
+import Listtodo from "../components/Listtodo";
+import { Button } from "bootstrap-react";
+import { useState } from "react";
 
 const Home = () => {
+  // iş ekleme butonunu açılışta ekranda göstermemek için show usaStateni tanımladım.
+  const [show, setShow] = useState(false);
+  // İş ekleme butonunun iş ekleme durumuna göre metin içeriğini değiştirdim.
+  const [text, ShowText] = useState("İş Ekle");
+
+  // butonun onclick eventine neler yaptıracağımızı tanımladık.
+  const change = (e) => {
+    setShow(!show);
+    const buttonText = show ? " İş Ekle" : "Gizle";
+    ShowText(buttonText);
+  };
+
   return (
     <div>
-        <Addtodo/>
-        <Listtodo/>
-    </div>
-  )
-}
+      <Button
+        onClick={() => change()}
+        className="fw-bolder"
+        variant="primary"
+        size="lg"
+      >
+        {text}
+      </Button>
 
-export default Home
+      {show && <Addtodo />}
+
+      <Listtodo />
+    </div>
+  );
+};
+
+export default Home;

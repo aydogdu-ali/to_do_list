c7b576f747289d586978a409c54e90a96cf50cc8
Ali <ali.ozgur@example.com>
2022-10-29 01:57:59 +0300

    set Api and get fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+
 import Addtodo from "../components/Addtodo";
 import Listtodo from "../components/Listtodo";
-import { Button } from "bootstrap-react";
-import { useState } from "react";
+import axios from "axios";
+import { useState,useEffect } from "react";
 
 const Home = () => {
   // iş ekleme butonunu açılışta ekranda göstermemek için show usaStateni tanımladım.
   const [show, setShow] = useState(false);
   // İş ekleme butonunun iş ekleme durumuna göre metin içeriğini değiştirdim.
   const [text, ShowText] = useState("İş Ekle");
+
+  // Api den gelen veriyi karşılamak için state tanımladım
+  const [task, setTask] = useState([])
+  const url ="https://63518c4d3e9fa1244e618a25.mockapi.io/api/list"
 
   // butonun onclick eventine neler yaptıracağımızı tanımladık.
   const change = (e) => {
@@ -17,20 +21,31 @@ const Home = () => {
     ShowText(buttonText);
   };
 
+// APİ den veri çekme fonksiyonu
+const getTask=async()=>{
+  //veriyi descrutring yaptım. 
+  const {data}=await axios(url)
+  setTask(data);
+  console.log(data)
+}
+
+// Api'den veriyi ilk önce bir kere çağırmak için
+// useEffect kullandım
+useEffect(() => {
+  getTask()
+},[])
+
+
   return (
     <div>
-      <Button
-        onClick={() => change()}
-        className="fw-bolder"
-        variant="primary"
-        size="lg"
-      >
-        {text}
-      </Button>
+       <button 
+      onClick={() => change()}
+      className=" btn btn-warning">{text}</button>
+          
 
       {show && <Addtodo />}
 
-      <Listtodo />
+      <Listtodo task = {task}/>
     </div>
   );
 };

40f98d1d541c111e3117d6575d27e13d17b21095
Ali <ali.ozgur@example.com>
2022-10-29 01:16:04 +0300

    finished Header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import picture from "../assets/picture.jpg";
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+ 
+
+  return (
+    <div className=" d-flex text-center justify-content-center mt-5 p-3">
+      <img className ="mt-1"src={picture} alt="" width="8%" />
+      <h4 className="mx-4 d-flex align-items-center text-danger fw-bolder">YOUR TO DO LİST TODAY</h4>
+    </div>
+  );
+};
+
+export default Header;

b1f8456a32d063b83e1317758293a3c3773caff3
Ali <ali.ozgur@example.com>
2022-10-28 19:04:22 +0300

    created components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Addtodo from '../components/Addtodo'
+import Listtodo from '../components/Listtodo'
+
+const Home = () => {
+  return (
+    <div>
+        <Addtodo/>
+        <Listtodo/>
+    </div>
+  )
+}
+
+export default Home

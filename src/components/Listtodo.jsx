efeaa2efbef190a4166d4a7234dcf7d8d2663ce0
Ali <ali.ozgur@example.com>
2022-10-29 04:05:35 +0300

    added delete input values

--- src/components/Listtodo.jsx
+++ src/components/Listtodo.jsx
@@ -1,21 +1,58 @@
+import axios from "axios";
 import React from "react";
-import {TiDelete} from "react-icons/ti";
-const Listtodo = ({ task }) => {
+import { TiDelete } from "react-icons/ti";
+
+// Api den gelen veriyi dom da yazdıran component
+const Listtodo = ({ task,getTask }) => {
+
+
+// listeden istediğimiz görevi sildirmek için api isteği gönderiyorum.
+const deleteTask = async (id)=>{
+
+  const url ="https://63518c4d3e9fa1244e618a25.mockapi.io/api/list"
+
+  try {
+    await axios.delete(`${url}/${id}`)
+
+  } catch (error) {
+    
+  }
+
+ getTask()
+}
+
+
+
+
+
+
+
+
   return (
-    <div className="container w-50">
-      <div>
+    <div className="container w-50 ">
+      <div className="p-3">
+        {/* gelen veriyi map yöntemi ile decstruc yapıyorum*/}
         {task.map((task) => {
           const { id, Date, list } = task;
           return (
-            <div key={id} className=" container mt-2 d-flex justify-content-between bg-info rounded-5">
+            <div
+              key={id}
+              className=" container mt-2 d-flex justify-content-between bg-info rounded-5 "
+            >
               <div className="text-center">
-           
-                <h4  className ="mt-4">{list}</h4>
-                <p className= "text-white fs-5 text-left">{Date}</p>
+                <h4 className="mt-4">{list}</h4>
+                <p className="text-white fs-5 text-left ">{Date}</p>
               </div>
-              <div className ="mt-4">
-                <TiDelete  style = {{cursor:"pointer",
-              marginRight:"2rem",color:"brown"}}size={35} />
+              <div className="mt-4">
+                <TiDelete
+                onClick={()=> deleteTask(id)}
+                  style={{
+                    cursor: "pointer",
+                    marginRight: "2rem",
+                    color: "brown",
+                  }}
+                  size={35}
+                />
               </div>
             </div>
           );
